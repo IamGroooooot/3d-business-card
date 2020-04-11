3882470a335465dc434f3ed0c383e96d4523a554
IamGroooooot <dury.ko@gmail.com>
2020-04-12 03:03:03 +0900

    Upload Unity Project
    동생 명함 디자인을 구현한 유니티 프로젝트 파일을 업로드 합니다.
    
    구현 특징
     - Line Render로 줄무늬 구현
     - 스마트폰 입력으로 명함 회전 가능
     - Shader 수정해서 3D Text가 게임오브젝트에 가려지도록 바꿈
    
    사용한 에셋
     - 구글 폰트에서 다운받은 Yeon_Sung Font
     - 유니티 에셋 스토어에서 다운받은 Wispy Sky

---
+++ Assets/Scripts/LineFitter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineFitter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] pos;
+    Vector3[] posVect;
+    private LineRenderer lr;
+    int i = 0;
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+        posVect = new Vector3[4];
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        i = 0;
+        foreach (var item in pos)
+        {
+            posVect[i] = item.transform.position;
+            i++;
+        }
+        lr.SetPositions(posVect);
+    }
+}
